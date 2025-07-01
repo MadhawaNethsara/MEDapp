174aa1bfeb5d6f6732ede688b7e6aba6781fbade
duvindu-ransika <duvinduransika100@gmail.com>
2025-07-01 10:33:30 +0530

    adding student dash board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Stureg from "./Admin/Stureg.jsx";
 import Updatet from "./Admin/Updatet.jsx";
 import Admin from "./Admin/Admin.jsx";
 import Layout from './Layout.jsx';
+import Sdash from "./Student/Sdash.jsx";
 
 function App() {
 
@@ -13,9 +14,9 @@ function App() {
    //<Login />
   //<Stureg />
     //<Updatet />
-  <Admin/>
+  //<Admin/>
     //<Layout/>
-
+    <Sdash/>
 
 
 
